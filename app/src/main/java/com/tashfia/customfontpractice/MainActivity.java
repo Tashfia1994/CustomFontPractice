f52d78b68654328bbf16c9d4f0cc1155e5f63b6f
Tashfia Islam <istila.tashfia@gmail.com>
2018-04-28 00:00:49 +0600

    Initial commit

---
+++ app/src/main/java/com/tashfia/customfontpractice/MainActivity.java
@@ -0,0 +1,47 @@
+package com.tashfia.customfontpractice;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+TextView txtText;
+EditText etxInput;
+Button btnSetText, btnReset;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtText=(TextView)findViewById(R.id.txt_text);
+        etxInput=(EditText)findViewById(R.id.etxt_input);
+        btnSetText=(Button)findViewById(R.id.btn_settext);
+        btnReset=(Button)findViewById(R.id.btn_reset);
+
+        Typeface tf=Typeface.createFromAsset(getAssets(),"GreatVibes-Regular.tff");
+             txtText.setTypeface(tf);
+                etxInput.setTypeface(tf);
+
+
+        btnSetText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text=etxInput.getText().toString();
+                txtText.setText(text);
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtText.setText("Text");
+                etxInput.setText("");
+            }
+        });
+    }
+}
